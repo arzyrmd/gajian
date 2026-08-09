0a7c5ccf2edb91d6c995fc8021a4015bfcb5c03d
arzyrmd <ategal505@gmail.com>
2026-08-09 23:10:18 +0700

    Expose sanitized environment variables on /info

--- api/info.php
+++ api/info.php
@@ -1,6 +1,17 @@
 <?php
 header('Content-Type: application/json');
+
+$env = [];
+foreach ($_ENV as $key => $value) {
+    if (str_contains(strtolower($key), 'password') || str_contains(strtolower($key), 'key') || str_contains(strtolower($key), 'secret')) {
+        $env[$key] = '********';
+    } else {
+        $env[$key] = $value;
+    }
+}
+
 echo json_encode([
     'php_version' => PHP_VERSION,
     'extensions' => get_loaded_extensions(),
+    'env' => $env,
 ]);
